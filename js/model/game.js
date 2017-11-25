a2127de88494018b87a63b771a178eb720850ec9
Hector Lopez <hlopez058@gmail.com>
2017-11-25 10:49:50 -0500

    changes to design

---
+++ js/model/game.js
@@ -0,0 +1,17 @@
+var game = {
+    player:null,
+    canvas:null,
+    score: null,
+    controller:null,
+    objects:null,
+    create : function(){
+      var obj = Object.create(this);
+      obj.player = null;
+      obj.canvas = null;
+      return obj;
+    },
+  
+    //add the game state information
+    //add the player state information
+    //add the canvas information
+  }
